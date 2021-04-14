68878d1a90b5bc4d1cbe6b2f62b5150a77c4a75d
Davide Russo <davide.russo.genova@gmail.com>
2021-04-14 18:48:09 +0200

    chore(angular): update to Angular 10

--- projects/ngx-picka-period/src/lib/date-pickers/base-picker.component.ts
+++ projects/ngx-picka-period/src/lib/date-pickers/base-picker.component.ts
@@ -1,6 +1,7 @@
-import {EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { EventEmitter, Input, OnInit, Output, Directive } from '@angular/core';
 import {NgxPickaPeriodConfig} from '../models/ngx-picka-period-config.model';
 
+@Directive()
 export abstract class BasePickerComponent implements OnInit {
   @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
   @Output() closePicker: EventEmitter<any> = new EventEmitter<any>();
